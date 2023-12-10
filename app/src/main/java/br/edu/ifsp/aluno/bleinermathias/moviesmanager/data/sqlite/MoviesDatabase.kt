3570709a9ac2f20436183a139fc79d35359425de
Bleiner Mathias <bleinerbbm@hotmail.com>
2023-12-10 19:42:49 -0300

    feat: Validating field movie

--- app/src/main/java/br/edu/ifsp/aluno/bleinermathias/moviesmanager/data/sqlite/MoviesDatabase.kt
+++ app/src/main/java/br/edu/ifsp/aluno/bleinermathias/moviesmanager/data/sqlite/MoviesDatabase.kt
@@ -6,6 +6,7 @@ import androidx.room.RoomDatabase
 import br.edu.ifsp.aluno.bleinermathias.moviesmanager.domain.entities.movie.MovieGenre
 import br.edu.ifsp.aluno.bleinermathias.moviesmanager.domain.entities.movie.Movie
 import br.edu.ifsp.aluno.bleinermathias.moviesmanager.domain.usecase.movie.MovieDAO
+import br.edu.ifsp.aluno.bleinermathias.moviesmanager.domain.usecase.movie.MovieGenreDAO
 
 @Database(entities = [Movie::class, MovieGenre::class], version = 1)
 abstract class MoviesDatabase: RoomDatabase() {
@@ -14,5 +15,6 @@ abstract class MoviesDatabase: RoomDatabase() {
     }
 
     abstract fun getMovieDao(): MovieDAO
+    abstract fun getMovieGenreDao(): MovieGenreDAO
 
 }
--- app/src/main/java/br/edu/ifsp/aluno/bleinermathias/moviesmanager/presentation/view/fragment/MovieFragment.kt
+++ app/src/main/java/br/edu/ifsp/aluno/bleinermathias/moviesmanager/presentation/view/fragment/MovieFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import br.edu.ifsp.aluno.bleinermathias.moviesmanager.R
@@ -18,6 +21,7 @@ import br.edu.ifsp.aluno.bleinermathias.moviesmanager.domain.entities.movie.Movi
 import br.edu.ifsp.aluno.bleinermathias.moviesmanager.domain.entities.movie.Movie.Companion.MOVIE_WATCHED_TRUE
 import br.edu.ifsp.aluno.bleinermathias.moviesmanager.presentation.view.fragment.MainFragment.Companion.EXTRA_MOVIE
 import br.edu.ifsp.aluno.bleinermathias.moviesmanager.presentation.view.fragment.MainFragment.Companion.MOVIE_FRAGMENT_REQUEST_KEY
+import br.edu.ifsp.aluno.bleinermathias.moviesmanager.presentation.viewModel.MovieViewModel
 import java.util.UUID
 
 
@@ -25,6 +29,13 @@ class MovieFragment : Fragment() {
 
     private lateinit var fragmentMovieBinding: FragmentMovieBinding
     private val navigationArgs: MovieFragmentArgs by navArgs()
+
+    private val spinnerDataList: MutableList<String> = mutableListOf()
+
+    private val movieViewModel: MovieViewModel by viewModels {
+        MovieViewModel.MovieViewModelFactory
+    }
+
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
@@ -33,6 +44,15 @@ class MovieFragment : Fragment() {
             getString(R.string.movie_details)
 
         fragmentMovieBinding = FragmentMovieBinding.inflate(inflater, container, false)
+
+
+        movieViewModel.spinnerDataList.observe(requireActivity()){genres ->
+            spinnerDataList.clear()
+            genres.forEachIndexed{index, genre ->
+                spinnerDataList.add(genre)
+            }
+        }
+        movieViewModel.getAllMovieGenres()
 
         val receivedMovie = navigationArgs.movie
 
@@ -70,8 +90,52 @@ class MovieFragment : Fragment() {
         }
 
         fragmentMovieBinding.run {
+            /* val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerDataList)
+             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+             spinnerGenre.adapter = adapter
+             spinnerGenre.setSelection(0)*/
+
             btnSaveMovie.setOnClickListener {
                 val checked = if (checkBoxWatched.isChecked) MOVIE_WATCHED_TRUE else MOVIE_WATCHED_FALSE
+
+                val movieName = editTextMovieName.text.toString()
+                val releaseYearText = editTextReleaseYear.text.toString()
+                val producer = editTextProducer.text.toString()
+                val durationText = editTextDuration.text.toString()
+                val ratingText = editTextRating.text.toString()
+                val selectedGenre = spinnerGenre.selectedItem.toString()
+
+                // Verificações e validações para cada campo
+                if (movieName.isBlank()) {
+                    showToast("Digite o nome do filme")
+                    return@setOnClickListener
+                }
+
+                val releaseYear = validateYear(releaseYearText)
+                if (releaseYear == null) {
+                    return@setOnClickListener
+                }
+
+                if (producer.isBlank()) {
+                    showToast("Digite o nome do produtor")
+                    return@setOnClickListener
+                }
+
+                val duration = validateIntField(durationText, "Duração inválida")
+                if (duration == null) {
+                    return@setOnClickListener
+                }
+
+                val rating = validateIntField(ratingText, "Classificação inválida")
+                if (rating == null) {
+                    return@setOnClickListener
+                }
+
+                if (selectedGenre.isBlank()) {
+                    showToast("Selecione um gênero")
+                    return@setOnClickListener
+                }
+
                 setFragmentResult(MOVIE_FRAGMENT_REQUEST_KEY, Bundle().apply {
                     putParcelable(
                         EXTRA_MOVIE, Movie(receivedMovie?.id ?: UUID.randomUUID().toString(),
@@ -110,7 +174,37 @@ class MovieFragment : Fragment() {
         return fragmentMovieBinding.root
     }
 
+    // Função para exibir uma mensagem Toast
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
 
+    // Função para validar o ano
+    private fun validateYear(yearText: String): Int? {
+        if (yearText.isBlank()) {
+            showToast("Digite o ano de lançamento")
+            return null
+        }
+        return try {
+            yearText.toInt()
+        } catch (e: NumberFormatException) {
+            showToast("Ano de lançamento inválido")
+            null
+        }
+    }
+
+    private fun validateIntField(valueText: String, errorMessage: String): Int? {
+        if (valueText.isBlank()) {
+            showToast(errorMessage)
+            return null
+        }
+        return try {
+            valueText.toInt()
+        } catch (e: NumberFormatException) {
+            showToast(errorMessage)
+            null
+        }
+    }
 
 
 }
